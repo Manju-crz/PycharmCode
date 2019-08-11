dda2bfdcc84f092890700407538bf9093701e0ef
unknown <manju.crz@gmail.com>
2019-08-11 22:11:30 +0530

    Code optimization 1.1

--- airbnb/pages/modules/search/SearchHomePage.py
+++ airbnb/pages/modules/search/SearchHomePage.py
@@ -3,7 +3,6 @@ import time as tm
 from interactor.mks.driver import BaseAUT as base
 from mks.utility import MksDates as mydates
 
-
 driver = base.driver
 
 SEARCHFORM_TITLE_TXT = "Book unique places to stay and things to do."
@@ -64,10 +63,7 @@ class SearchForm:
     def expand_checkin_calendar(self):
         driver.find_element(By.XPATH, self.CHECKIN_CALENDAR_EDIT_FIELD_XPATH).click()
         tm.sleep(1)
-        if driver.find_element(By.XPATH, self.CALENDAR_XPATH).is_displayed():
-            return True
-        else:
-            False
+        return driver.find_element(By.XPATH, self.CALENDAR_XPATH).is_displayed()
 
     def expand_checkout_calendar(self):
         driver.find_element(By.XPATH, self.CHECKOUT_CALENDAR_EDIT_FIELD_XPATH).click()
@@ -99,14 +95,10 @@ class SearchForm:
 
     def select_calendar(self, date_to_select, is_check_in=False):
         if is_check_in:
-            if self.expand_checkin_calendar():
-                pass
-            else:
+            if self.expand_checkin_calendar() is not True:
                 raise RuntimeError("Check in calendar has not displayed in the Ui to select date")
         else:
-            if self.expand_checkout_calendar():
-                pass
-            else:
+            if self.expand_checkout_calendar() is not True:
                 raise RuntimeError("Check out calendar has not displayed in the Ui to select date")
         d, m, y = mydates.get_date_monthname_year_values_from_date(date_to_select)
         if search_form.select_opened_calendar_date_month_year(d, m, y):
@@ -131,8 +123,7 @@ class SearchForm:
         guest = driver.find_element(By.XPATH, self.GUEST_SELECTED_LABEL_XPATH).text
         if str(guest).strip() == str(adults + children + infants) + " guests":
             return True
-        else:
-            return False
+        return False
 
     def get_search_form_title(self):
         return driver.find_element(By.XPATH, self.SEARCHFORM_TITLE_XPATH).text
@@ -142,21 +133,14 @@ search_form = SearchForm()
 
 
 def submit_search_hotels_form(city_name, from_date, to_date, adults_count, children_count, infants_count):
-    if search_form.accept_cookies_popup():
-        pass
-    else:
+    if search_form.accept_cookies_popup() is not True:
         raise RuntimeError("Even after accepting cookies popup, still the popup is displayed")
-
-    if search_form.select_location(city_name):
-        pass
-    else:
+    if search_form.select_location(city_name) is not True:
         raise ValueError("Either given selectable value " + city_name + "is not existing in the drop down, or not "
                                                                         "selected successfully even after selected")
     search_form.select_calendar(from_date, True)
     search_form.select_calendar(to_date)
-    if search_form.select_guests(adults_count, children_count):
-        pass
-    else:
+    if search_form.select_guests(adults_count, children_count) is not True:
         raise RuntimeError("All guests are not selected in the drop down box successfully")
     driver.find_element(By.XPATH, search_form.SEARCH_BUTTON_XPATH).click()
 
